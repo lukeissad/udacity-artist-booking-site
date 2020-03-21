c48cb728efac4b8664db2d3f8a7113079b298634
Lucas Sadzik <ritsuboshi@gmail.com>
2020-03-21 23:31:37 +0100

    Add create_show_submission logic
    Show data from a submitted form is inserted into the database.

--- app.py
+++ app.py
@@ -541,15 +541,34 @@ def create_shows():
 
 @app.route('/shows/create', methods=['POST'])
 def create_show_submission():
+    error = False
+    try:
+        show = Show(
+        venue_id=request.form['venue_id'],
+        artist_id=request.form['artist_id'],
+        start_time=request.form['start_time']
+        )
+        db.session.add(show)
+        db.session.commit()
+    except:
+        error = True
+        db.session.rollback()
+        print(sys.exc_info())
+    finally:
+        db.session.close()
+        if error:
+            flash('An error occurred. Show could not be listed.')
+        else:
+            flash('Show was successfully listed!')
+        return render_template('pages/home.html')
   # called to create new shows in the db, upon submitting new show listing form
   # TODO: insert form data as a new Show record in the db, instead
 
   # on successful db insert, flash success
-  flash('Show was successfully listed!')
+  # flash('Show was successfully listed!')
   # TODO: on unsuccessful db insert, flash an error instead.
   # e.g., flash('An error occurred. Show could not be listed.')
   # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
-  return render_template('pages/home.html')
 
 @app.errorhandler(404)
 def not_found_error(error):
